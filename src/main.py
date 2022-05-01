d79bb5997017e2c3a5f797e48ae898ac066a3f61
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-05-01 16:59:03 +0200

    Made everything PEP8

--- src/main.py
+++ src/main.py
@@ -17,4 +17,3 @@ for i in range(len(dataset_x)):
 
 input_ = [3, 30]
 print(perc.output_perc(input_))
-
--- src/perceptron.py
+++ src/perceptron.py
@@ -1,33 +1,27 @@
 weight = [2,-2]
 sum = 0
+
+
 class Perceptron:
     def __init__(self):
-        self.sum = sum
+        self.sum    = sum
         self.weight = weight
 
 
     def sign(self, n):
         self.n = n
-        if n >=0:
-            return 1
-        else:
-            return -1
+        return 1 if n >= 0 else -1
     
     
-    def output_perc(self, input=[2]):
+    def output_perc(self, input_=[2]):
         for i in range(len(self.weight)):
-            self.sum += input[i]*self.weight[i]
-        output = self.sign(self.sum)
-        return output
+            self.sum += input_[i] * self.weight[i]
+        
+        return self.sign(self.sum)
 
-    def train(self, input, output, target):
-        lr = 0.106
+
+    def train(self, input_, output, target):
+        lr    = 0.106
         error = target - output
         for i in range(len(self.weight)):
-            self.weight[i] += error * input[i] * lr
-
-            
-
-
-
-
+            self.weight[i] += error * input_[i] * lr
