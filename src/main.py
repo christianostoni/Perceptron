d249b8e57368ead26ffa7d5ad12930cc4761925e
Christian Ostoni <86129489+christianostoni@users.noreply.github.com>
2022-05-03 19:52:11 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ dataset_y = [70, 50, 74.6, 49.4, 74.6, 48.3, 72.8, 45.8, 91.4, 56.6]
 
 for i in range(len(dataset_x)):
     input_ = [dataset_x[i], dataset_y[i]]
-    target = 1 if dataset_y[i] >= 60 else -1
+    target = -1 if dataset_y[i] >= 60 else 1
     output = perc.output_perc(input_)
     
     perc.train(input_, output, target)
